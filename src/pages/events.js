9c41ddbea32bab1b221155dc6405013f0f4da18a
Bradley Goldsmith <bradleygoldsmith14@gmail.com>
2024-04-11 16:30:51 -0400

    add images to events

--- src/pages/events.js
+++ src/pages/events.js
@@ -4,8 +4,8 @@ import Layout from "../components/layout.js";
 import MobileLayout from "../components/MobileLayout.js";
 import VariableContext from "../context/VariableProvider.js";
 import { PortableText } from "@portabletext/react";
-import BlogPost from "../components/BlogPost.js";
 import * as styles from "../styles/blogPostStyles.module.css";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 const Events = () => {
 	const { isMobile } = useContext(VariableContext);
@@ -14,36 +14,54 @@ const Events = () => {
         allSanityEventPost {
             nodes {
               _rawBody
+			  image {
+				asset {
+				  gatsbyImageData
+				}
+			  }
             }
           }
         }
     `);
+	let key = 0;
 
 	if (isMobile) {
 		return (
 			<MobileLayout>
-				<div style={{ marginTop: "10vh" }}>
-					{data.allSanityEventPost.nodes.map(eventPost => (
-						<div className={styles.post}>
-							<div className={styles.postBody}>
-								<PortableText value={eventPost._rawBody} />
+				<div style={{ marginTop: "10vh" }} key={key++}>
+					{data.allSanityEventPost.nodes.map(eventPost => {
+						const img = getImage(eventPost.image.asset.gatsbyImageData);
+						return (
+							<div className={styles.post} key={key++}>
+								<div className={styles.image}>
+									<GatsbyImage image={img} alt="Event Image" />
+								</div>
+								<div className={styles.postBody}>
+									<PortableText value={eventPost._rawBody} />
+								</div>
 							</div>
-						</div>
-					))}
+						);})}
 				</div>
 			</MobileLayout>
 		);
 	}
+
+	console.log(data);
 	return (
 		<Layout showBanner={false}>
 			<div style={{ marginTop: "10vh" }}>
-				{data.allSanityEventPost.nodes.map(eventPost => (
-					<div className={styles.post}>
-						<div className={styles.postBody}>
-							<PortableText value={eventPost._rawBody} />
+				{data.allSanityEventPost.nodes.map(eventPost => {
+					const img = getImage(eventPost.image.asset.gatsbyImageData);
+					return (
+						<div className={styles.post} key={key++}>
+							<div className={styles.image}>
+								<GatsbyImage image={img} alt="Event Image" />
+							</div>
+							<div className={styles.postBody}>
+								<PortableText value={eventPost._rawBody} />
+							</div>
 						</div>
-					</div>
-				))}
+					);})}
 			</div>
 		</Layout>
 	);
