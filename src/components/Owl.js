cc42b1d7007f2dfa8eb74a1986a20beb9b1e8766
Bradley Goldsmith <bradleygoldsmith14@gmail.com>
2024-04-12 17:19:32 -0400

    fix flicker bug

--- src/components/Owl.js
+++ src/components/Owl.js
@@ -10,19 +10,65 @@
         // setFadeOut(true);
 */
 
-import React from "react";
+import React, { useState } from "react";
 import { StaticImage } from "gatsby-plugin-image";
 import * as styles from "../styles/OwlStyles.module.css";
 import { AnimatePresence, motion } from "framer-motion";
+import { isChrome } from "react-device-detect";
 
 const Owl = ({ showOwl, setShowOwl, isMobile }) => {
-	// const [fadeOut, setFadeOut] = useState(false); // for css fade out
+	const [fadeOut, setFadeOut] = useState(false); // for css fade out
 
 	const closeModal = () => {
-		sessionStorage.setItem("hasOwlBeenShown", true);
-		setShowOwl(false);
+		if (isChrome) {
+			sessionStorage.setItem("hasOwlBeenShown", true);
+			setShowOwl(false);
+		} else {
+			setTimeout(() => {
+				sessionStorage.setItem("hasOwlBeenShown", true);
+				setShowOwl(false);
+			}, 500); // Adjust delay as needed
+			setFadeOut(true);
+		}
 	};
 
+	// Animate Presence causes a flicker in Safari and firefox, so use a regular
+	// css transition class to handle fade on exit for those browsers.
+	// Unclear whether bug is part of framer motion or not. 
+	if (!isChrome) {
+		if (isMobile) {
+			return (
+				<div className={`${styles.overlay} ${styles.mobile} ${fadeOut ? styles.fadeOut : ""}`}
+					key="modal"
+					onClick={closeModal}>
+					
+					<StaticImage 
+						alt="Big Owl pop up"
+						src={"../images/BIGOWL.png"}
+						className={`${styles.centeredImage} ${styles.mobile}`} 
+					/>
+					<div className={styles.textBox}>
+							ENTER
+					</div>  
+				</div>
+			);
+		}
+		return (
+			<div className={`${styles.overlay}  ${fadeOut ? styles.fadeOut : ""}`}
+				onClick={closeModal}>
+				<StaticImage 
+					alt="Big Owl pop up"
+					src={"../images/BIGOWL.png"}
+					className={styles.centeredImage}
+				/>
+				<div className={styles.textBoxOverlay}>
+					<div className={styles.textBox}>
+							ENTER
+					</div>
+				</div>
+			</div>
+		);
+	}
 	if (isMobile) {
 		return (
 			<AnimatePresence>
