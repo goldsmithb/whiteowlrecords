02b8e502a7a71bc7d80232eaa8dc6567fbe80718
Bradley Goldsmith <bradleygoldsmith14@gmail.com>
2024-10-07 16:36:36 -0400

    owl bug

--- src/components/MobileLayout.js
+++ src/components/MobileLayout.js
@@ -5,54 +5,50 @@ import HamburgerMenu from "../components/HamburgerMenu.js";
 import Owl from "../components/Owl.js";
 
 const MobileLayout = ({ children }) => {
-	const [showOwl, setShowOwl] = useState(false);
-	const updateShowOwl = (b) => setShowOwl(b);
+  const [showOwl, setShowOwl] = useState(true);
+  const updateShowOwl = (b) => setShowOwl(b);
 
-	useEffect(() => {
-		const val = sessionStorage.getItem("hasOwlBeenShown");
-		let hasOwlBeenShown = val === "true" ? true : false;
-		// show the owl if it has not yet been shown
-		setShowOwl(!hasOwlBeenShown);
-	}, []);
-    
-	if (showOwl) {
-		return (
-			<Owl updateShowOwl={updateShowOwl} isMobile={true} />
-		);
-	} else {
-		return (
-			<>
-				<div className={`${styles.topBar}`}>
-					<div className={styles.bannerWrapper}>
-						<Banner isMobile={true} />
-					</div>
-					<HamburgerMenu isMobile={true}/>
-				</div>
+  useEffect(() => {
+    const val = sessionStorage.getItem("hasOwlBeenShown");
+    let hasOwlBeenShown = val === "true" ? true : false;
+    // show the owl if it has not yet been shown
+    setShowOwl(!hasOwlBeenShown);
+  });
 
-				<div className={styles.middleBar}>
-					{children}
-				</div>
-			</>
-		);
-	}
-	// return (
-	// 	<>
-	// 		<Owl updateShowOwl={updateShowOwl} isMobile={true} />
-            
-	// 		<div className={`${styles.topBar} ${showOwl && styles.hideTopBar}`}>
-	// 			<div className={styles.bannerWrapper}>
-	// 				<Banner isMobile={true} />
-	// 			</div>
-	// 			<HamburgerMenu isMobile={true}/>
-	// 		</div>
+  if (showOwl) {
+    return <Owl updateShowOwl={updateShowOwl} isMobile={true} />;
+  } else {
+    return (
+      <>
+        <div className={`${styles.topBar}`}>
+          <div className={styles.bannerWrapper}>
+            <Banner isMobile={true} />
+          </div>
+          <HamburgerMenu isMobile={true} />
+        </div>
 
-	// 		<div className={styles.middleBar}>
-	// 			{!showOwl && children}
-	// 		</div>
-            
-	// 		{/* <BottomBar />   -- IMO this should only appear in the about page on mobile view.*/}
-	// 	</>
-	// );
+        <div className={styles.middleBar}>{children}</div>
+      </>
+    );
+  }
+  // return (
+  // 	<>
+  // 		<Owl updateShowOwl={updateShowOwl} isMobile={true} />
+
+  // 		<div className={`${styles.topBar} ${showOwl && styles.hideTopBar}`}>
+  // 			<div className={styles.bannerWrapper}>
+  // 				<Banner isMobile={true} />
+  // 			</div>
+  // 			<HamburgerMenu isMobile={true}/>
+  // 		</div>
+
+  // 		<div className={styles.middleBar}>
+  // 			{!showOwl && children}
+  // 		</div>
+
+  // 		{/* <BottomBar />   -- IMO this should only appear in the about page on mobile view.*/}
+  // 	</>
+  // );
 };
 
 export default MobileLayout;
--- src/components/layout.js
+++ src/components/layout.js
@@ -5,38 +5,42 @@ import HamburgerMenu from "../components/HamburgerMenu.js";
 import Owl from "../components/Owl.js";
 
 const Layout = ({ children }) => {
-	const [showOwl, setShowOwl] = useState(false);
-	const updateShowOwl = (b) => {
-		console.log("setting " + b); setShowOwl(b);
-	};
-	useEffect(() => {
-		const val = sessionStorage.getItem("hasOwlBeenShown");
-		let hasOwlBeenShown = val === "true" ? true : false;
-		// show the owl if it has not yet been shown
-		setShowOwl(!hasOwlBeenShown);
-	}, []);
-    
+  const [showOwl, setShowOwl] = useState(true);
+  const updateShowOwl = (b) => {
+    console.log("setting " + b);
+    setShowOwl(b);
+  };
+  useEffect(() => {
+    const val = sessionStorage.getItem("hasOwlBeenShown");
+    let hasOwlBeenShown = val === "true" ? true : false;
+    // show the owl if it has not yet been shown
+    setShowOwl(!hasOwlBeenShown);
+  });
 
-	return (
-		<div className={styles.siteWrap}>
-			{showOwl && <Owl updateShowOwl={updateShowOwl} />}
-			{/* todo: do this in a cleaner way =- but do not render components hidden behind owl! */}
-			{!showOwl && (<>
-				<div className={styles.leftCol}>
-					<HamburgerMenu />
-				</div>
+  if (showOwl) {
+    return (
+      <div className={styles.siteWrap}>
+        {showOwl && <Owl updateShowOwl={updateShowOwl} />}
+      </div>
+    );
+  }
 
-				<div className={styles.middleCol}>
-					{children}
-				</div>
+  return (
+    <div className={styles.siteWrap}>
+      {
+        <>
+          <div className={styles.leftCol}>
+            <HamburgerMenu />
+          </div>
 
-				<div className={styles.rightCol}>
-				</div>
-				<BottomBar />
-			</>)}
-			
-		</div>
-	);
+          <div className={styles.middleCol}>{children}</div>
+
+          <div className={styles.rightCol}></div>
+          <BottomBar />
+        </>
+      }
+    </div>
+  );
 };
 
 export default Layout;
